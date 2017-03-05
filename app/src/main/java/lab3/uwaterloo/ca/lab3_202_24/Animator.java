0f1ae08e50e56cc1f0cf8d8b1fe3d001ba71e2f1
PatzHum <patrick.hum@hotmail.com>
2017-03-05 11:16:59 -0500

    Animation Class

---
+++ app/src/main/java/lab3/uwaterloo/ca/lab3_202_24/Animator.java
@@ -0,0 +1,14 @@
+package lab3.uwaterloo.ca.lab3_202_24;
+
+/**
+ * Created by patri on 2017-03-05.
+ */
+
+interface Movment{
+    public void setLocation(int x, int y);
+    public int getX();
+    public int getY();
+}
+
+public class Animator {
+}
