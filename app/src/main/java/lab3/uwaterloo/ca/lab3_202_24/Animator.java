3da5fb5f63655b1436b1a9a1691e4da6c582c40f
PatzHum <patrick.hum@hotmail.com>
2017-03-05 12:29:18 -0500

    PixelRename

--- app/src/main/java/lab3/uwaterloo/ca/lab3_202_24/Animator.java
+++ app/src/main/java/lab3/uwaterloo/ca/lab3_202_24/Animator.java
@@ -1,14 +1,12 @@
 package lab3.uwaterloo.ca.lab3_202_24;
 
-import android.util.Pair;
-
 /**
  * Created by patri on 2017-03-05.
  */
 
 interface Movement{
-    public void setX(int x);
-    public void setY(int y);
+    public void setPixelX(int x);
+    public void setPixelY(int y);
     public int getPixelX();
     public int getPixelY();
 }
@@ -43,17 +41,17 @@ public class Animator {
         Vy += acc;
 
         if (Math.abs(dx) < Vx){
-            contextObject.setX(targetX);
+            contextObject.setPixelX(targetX);
             Vx = 0;
         }else{
-            contextObject.setY((int)(cx + Vx));
+            contextObject.setPixelY((int)(cx + Vx));
         }
         
         if (Math.abs(dy) < Vy){
-            contextObject.setY(targetY);
+            contextObject.setPixelY(targetY);
             Vy = 0;
         }else{
-            contextObject.setY((int)(cy + Vy));
+            contextObject.setPixelY((int)(cy + Vy));
         }
     }
 }
