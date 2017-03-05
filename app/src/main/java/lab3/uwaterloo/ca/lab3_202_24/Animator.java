15d828e2b3bf811ec757b0b7848da08c679dc19e
PatzHum <patrick.hum@hotmail.com>
2017-03-05 11:17:45 -0500

    I can't  spell

--- app/src/main/java/lab3/uwaterloo/ca/lab3_202_24/Animator.java
+++ app/src/main/java/lab3/uwaterloo/ca/lab3_202_24/Animator.java
@@ -4,11 +4,14 @@ package lab3.uwaterloo.ca.lab3_202_24;
  * Created by patri on 2017-03-05.
  */
 
-interface Movment{
+interface Movement{
     public void setLocation(int x, int y);
     public int getX();
     public int getY();
 }
 
 public class Animator {
+    Animator(Movement object){
+
+    }
 }
