215b16aefeab5e710cb3c7c225d7d891da569907
PatzHum <patrick.hum@hotmail.com>
2017-03-05 15:20:56 -0500

    Finalized
    Forced portrait screen orientation
    Commented Animation class

--- app/src/main/java/lab3/uwaterloo/ca/lab3_202_24/Animator.java
+++ app/src/main/java/lab3/uwaterloo/ca/lab3_202_24/Animator.java
@@ -1,12 +1,12 @@
 package lab3.uwaterloo.ca.lab3_202_24;
 
-import android.util.Log;
-
 /**
  * Created by patri on 2017-03-05.
  */
 
-interface Movement{
+interface Movement
+/*Movement Interface for getting and setting locations of an object*/
+{
     public void setPixelX(int x);
     public void setPixelY(int y);
     public int getPixelX();
@@ -14,11 +14,10 @@ interface Movement{
 }
 
 public class Animator {
-    Movement contextObject;
-    private int targetX, targetY;
+    Movement contextObject;             //Object to move
+    private int targetX, targetY;       //Destination
 
-    private float Vx, Vy;
-    private float acc = (float) 2;
+    private float Vx, Vy;                       //Current velocity
 
     Animator(Movement object){
         this.contextObject = object;
@@ -27,7 +26,9 @@ public class Animator {
     }
 
     public void setTarget(int x, int y){
-        if (Vx == 0 && Vy == 0) {
+        if (Vx == 0 && Vy == 0)
+        //Ensure that there is no velocity at the moment.
+        {
             targetX = x;
             targetY = y;
             Vx = 0;
@@ -35,31 +36,39 @@ public class Animator {
         }
     }
 
-    public void tick(){
+    public void tick()
+    /**
+     * Tick handles the per-cycle animation
+     **/
+    {
         int cx = contextObject.getPixelX();
         int cy = contextObject.getPixelY();
 
+        //Calculate delta locations
         int dx = targetX - cx;
         int dy = targetY - cy;
 
+        final float ACCELERATION = (float) 2;       //Acceleration constant
+
+        //If the delta is non-zero, add acceleration to the velocity
         if (dx != 0) {
-            Vx += acc * (dx < 0 ? -1 : 1);
+            Vx += ACCELERATION * (dx < 0 ? -1 : 1);
         }else{
             Vx = 0;
         }
         if (dy != 0) {
-            Vy += acc * (dy < 0 ? -1 : 1);
+            Vy += ACCELERATION * (dy < 0 ? -1 : 1);
         }else{
             Vy = 0;
         }
 
+        //If the delta is close enough to the final destination jump there.
         if (Math.abs(dx) < Math.abs((int) (2 * Vx))){
             contextObject.setPixelX(targetX);
             Vx = 0;
         }else{
             contextObject.setPixelX((int)(cx + Vx));
         }
-        
         if (Math.abs(dy) < Math.abs((int) (2 * Vy))){
             contextObject.setPixelY(targetY);
             Vy = 0;
